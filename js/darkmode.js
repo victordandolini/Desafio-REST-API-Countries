900b7be4de89831f914edea41b425c3a435bda03
Victor Dandolini <victordandolini@hotmail.com>
2021-10-21 17:49:29 -0300

    1 parte

---
+++ js/darkmode.js
@@ -0,0 +1,12 @@
+const btnDarkmode = document.querySelector('.btn-darkmode');
+
+btnDarkmode.addEventListener('click', () => {
+    console.log('clickou')
+    document.body.classList.toggle('darkmode');
+    
+    if(document.body.className === 'darkmode'){
+    btnDarkmode.innerHTML = `<i class="far fa-sun"></i> Light Mode` 
+} else {
+        btnDarkmode.innerHTML = `<i class="far fa-moon"></i> Dark Mode`
+    }
+})
