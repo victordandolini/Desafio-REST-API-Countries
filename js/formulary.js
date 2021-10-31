47a2813c0fa0669683f896cf84ac6103f179ab00
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-10-31 00:20:13 -0300

    correção acento

--- js/formulary.js
+++ js/formulary.js
@@ -2,12 +2,21 @@ const formulary = document.getElementById('formulary');
 const inputFormulary = document.getElementById('input-fomulary');
 
 const formularyClient = data => { 
+    function removeAcento(text){       
+        text = text.toLowerCase();                                                         
+        text = text.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+        text = text.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+        text = text.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+        text = text.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+        text = text.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+        return text;                 
+    }
     formulary.addEventListener('keyup', e => {
         e.preventDefault()
-        const inputClient = inputFormulary.value.toLowerCase()
+        const inputClient = removeAcento(inputFormulary.value.toLowerCase())
         console.log(inputClient);
         const arrayFiltrado = data.filter(item => {
-            const input = item.translations.por.common.toLowerCase();
+            const input = removeAcento(item.translations.por.common.toLowerCase());
             if(input.indexOf(inputClient) !== -1){
                 return item
             }
